f0aaa2d6cae8744e914832b77c78b71ff1650cfc
troyhagen8 <31894976+troyhagen8@users.noreply.github.com>
2017-11-02 16:59:37 -0400

    Added review model and controllers along with views

---
+++ MvcMovie/MvcMovie/Models/Review.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Review
+    {
+        public int ID { get; set; }
+        public String ReviewerName { get; set; }
+        public String comment { get; set; }
+    }
+}
