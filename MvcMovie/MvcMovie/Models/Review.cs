f20a220c1606348bbe88b88da5c2174fd3286d5d
troyhagen8 <31894976+troyhagen8@users.noreply.github.com>
2017-11-06 03:20:20 -0500

    Added Foriegn Key to Review Controller

--- MvcMovie/MvcMovie/Models/Review.cs
+++ MvcMovie/MvcMovie/Models/Review.cs
@@ -1,14 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
     public class Review
-    {
+    { 
+
         public int ID { get; set; }
+    
+        public int MovieID { get; set; }
+
+        [Display(Name="Your name")]
         public String ReviewerName { get; set; }
-        public String comment { get; set; }
+
+        public String Comment { get; set; }
     }
 }
