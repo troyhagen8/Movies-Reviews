b6f763f59b83d466f66f5d23f55f0203420f3913
hagent2 <hagent2@D017LAB-15>
2017-11-09 14:08:40 -0500

    k;wjgv

---
+++ MvcMovie/Migrations/20171109181138_Review_2.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MvcMovie.Migrations
+{
+    public partial class Review_2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Review",
+                table: "Review");
+
+            migrationBuilder.DropColumn(
+                name: "ID",
+                table: "Review");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ReviewID",
+                table: "Review",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Review",
+                table: "Review",
+                column: "ReviewID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Review",
+                table: "Review");
+
+            migrationBuilder.DropColumn(
+                name: "ReviewID",
+                table: "Review");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ID",
+                table: "Review",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Review",
+                table: "Review",
+                column: "ID");
+        }
+    }
+}
---
+++ MvcMovie/Models/Review.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+    public class Review
+    { 
+
+        public int ReviewID { get; set; }
+    
+        public int MovieID { get; set; }
+
+        [Display(Name="Your name")]
+        public String ReviewerName { get; set; }
+
+        public String Comment { get; set; }
+    }
+}
